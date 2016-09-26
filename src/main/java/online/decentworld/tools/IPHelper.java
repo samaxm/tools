7aa0326b611cc47d1be33bce231a19feafff240b
samaxm <846452512@qq.com>
2016-09-26 14:55:48 +0800

    add environment control

---
+++ src/main/java/online/decentworld/tools/IPHelper.java
@@ -0,0 +1,28 @@
+package online.decentworld.tools;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Sammax on 2016/9/26.
+ */
+public class IPHelper {
+    public static String getLocalIP(HttpServletRequest request){
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+}
